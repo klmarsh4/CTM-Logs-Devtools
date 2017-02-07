68a7810a73c421355270c43c1f093af709413a8e
Kevin Marsh <kmarsh@corp.versionone.net>
2017-02-07 10:35:21 -0500

    include the ability to censor logs
    -- currently the code always attempt to censor and what is censored is determined by the code
    -- the next 2 goals are to conditionally do this based on user input

--- popup.js
+++ popup.js
@@ -35,6 +35,7 @@ function getLog(coreUrl, service){
 
 function writeLogsToPopup(resp){
   if (this.readyState == 4 && this.status == 200) {
+    console.log(this)
     var popup = document.getElementById("log_body");
     var logStart = this.responseText.indexOf("<pre>");
     var logEnd = this.responseText.indexOf("</pre>");
@@ -44,8 +45,44 @@ function writeLogsToPopup(resp){
       if(serviceStart >= 0)
         logs = "<pre>" + logs.substring(serviceStart+39) //get rid of the #######################################
     }
-    popup.innerHTML = logs;
+    censoredLogs = censorLogLevels(logs)
+    console.log("censoredLogs", censoredLogs)
+    popup.innerHTML = censoredLogs;
   }
+}
+
+function censorLogLevels(logs){
+  debugger;
+  var includedLevels = ["ERROR"];
+  var excludedLevels = ["DEBUG", "INFO", "WARNING", "CRITICAL"];
+  logsByLine = logs.split('\n')
+  console.log("logsByLine", logsByLine);
+  newLogs = logsByLine[0] + '\n'; //include the first line, it contains the <pre> and info about the log
+  var includedPrev = false;
+  for (var k = 1; k < logsByLine.length - 1; k++){
+    var line = logsByLine[k];
+    var include = false;
+    for (var i = 0; i < includedLevels.length && include === false; i++){
+      if(line.indexOf(includedLevels[i]) > -1){
+        include = true;
+      }
+    }
+    if (!include){
+      include = includedPrev; //include the line if the previous line was included, as it might be an extension
+                              //unless the line contains a log level
+      for(var i = 0; i < excludedLevels.length && include === true; i++){
+        if(line.indexOf(excludedLevels[i]) > -1){
+          include = false;
+        }
+      }
+    }
+    if(include){
+      newLogs += (line + '\n');
+    }
+    includedPrev = include;
+  }
+  newLogs += logsByLine[logsByLine.length - 1]; //include </pre>
+  return newLogs;
 }
 
 document.addEventListener('DOMContentLoaded', populateLogs);
